90ae6a5e4fc411d7d5d2333d6a4713616a02916a
pkmnnerd <b_liu72@yahoo.com>
2015-06-26 10:32:18 -0400

    Added pathway images and limiting by organism

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,7 +9,7 @@
     // Later, display an error to the user. For now, display an error on the developer console.
     console.error('Search returned error! Status=' + json.obj.status + ' Message=' + json.obj.message);
     console.log(json);
-    $('.data', appContext).html('Error loading data :(');
+    $('.data', appContext).html(json.obj.message);
   };
 
   var showSearchResult1 = function(json) {
@@ -20,13 +20,15 @@
   	}
     //console.log(json.obj.result);
     var html = '<table> <thead><tr>'+
-                '<th>KEGG Pathway ID</th> <th>KEGG Pathway Name</th>' +
+                '<th>KEGG Pathway ID</th> <th>KEGG Pathway Name</th> <th>Pathway Map</th>' +
                 '</tr></thead><tbody>';
 
     for (var i = 0; i < json.obj.result.length; i++) {
       var entry = json.obj.result[i];
       //console.log(json.obj.result[entry]);
-      html += '<tr><td>' + entry.identifier + '</td><td>' + entry.name + '</td></tr>\n';
+      html += '<tr><td>' + entry.identifier + '</td><td>' + entry.name + '</td>\n' +
+              '<td><button type="button" class="btn btn-default btn-xs" '+ 'data-toggle="modal" data-target="#exampleModal" '+
+              'data-id="' + entry.identifier + '" data-name="' + entry.name + '">Show</button></td></tr>\n'
     }
     html += '</tbody></table>';
     console.log('hello');
@@ -34,12 +36,48 @@
     $('.data table', appContext).DataTable();
   };
 
+
+  $('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    var id = button.data('id'); // Extract info from data-* attributes
+    var name = button.data('name');
+
+    var modal = $(this)
+    modal.find('.modal-title').text('Pathway Map of ' + name);
+    modal.find('.modal-body').html('<img src="http://rest.kegg.jp/get/map' + id + '/image">')
+  })
+
+
   window.addEventListener('Agave::ready', function() {
     var Agave;//, help, helpItem, helpDetail, methods, methodDetail;
 
     Agave = window.Agave;
 
     //appContext.html('<h2>Hello AIP Science App &plus; Agave API!</h2><div class="api-help list-group"></div><hr><div class="api-info"></div><br>');
+
+    $('form[name=taxon_form]').on('submit', function(e) {
+      e.preventDefault();
+      $('.data', appContext).html("Reloading...");
+
+      var query = {
+        'taxon_id': this.taxonId.value,
+      };
+
+      if (this.taxonId.value === '') {
+        query = {};
+      }
+
+      $('.gene_results').empty();
+      $('.error').empty();
+      Agave.api.adama.search(
+                {'namespace': 'bliu',
+      	   'service': 'pathway_v0.2',
+      	   'queryParams': query},
+      	  showSearchResult1,
+      	  showSearchError
+            );
+    });
+
 
 
     Agave.api.adama.search(
