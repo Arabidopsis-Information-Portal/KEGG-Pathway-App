1a463911ba9260122b99a4e54d14921b20eeacb9
pkmnnerd <b_liu72@yahoo.com>
2015-06-29 15:36:57 -0400

    Added expanding a pathway to list all accessible fields

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,6 @@
 (function(window, $, undefined) {
   'use strict';
 
-  console.log('Hello, testapp!');
-
   var appContext = $('[data-app-name="testapp"]');
 
   var showSearchError = function(json) {
@@ -12,29 +10,9 @@
     $('.data', appContext).html(json.obj.message);
   };
 
-  var showSearchResult1 = function(json) {
-  	// JavaScript === and !== operators test value and type.
-  	if (json.obj.status !== 'success') {
-  	    console.log('Search result status is NOT good!');
-  	    return (false);
-  	}
-    //console.log(json.obj.result);
-    var html = '<table class="table hover row-border stripe"> <thead><tr>'+
-                '<th>KEGG Pathway ID</th> <th>KEGG Pathway Name</th> <th>Pathway Map</th>' +
-                '</tr></thead><tbody>';
 
-    for (var i = 0; i < json.obj.result.length; i++) {
-      var entry = json.obj.result[i];
-      //console.log(json.obj.result[entry]);
-      html += '<tr><td>' + entry.identifier + '</td><td>' + entry.name + '</td>\n' +
-              '<td><button type="button" class="btn btn-default btn-xs" '+ 'data-toggle="modal" data-target="#exampleModal" '+
-              'data-id="' + entry.identifier + '" data-name="' + entry.name + '" data-org="' + entry.organism + '">Show</button></td></tr>\n';
-    }
-    html += '</tbody></table>';
-    console.log('hello');
-    $('.data', appContext).html(html);
-    $('.data table', appContext).DataTable({'columnDefs': [ { 'targets': 2, 'orderable': false } ]});
-  };
+
+
 
 
   $('#exampleModal').on('show.bs.modal', function (event) {
@@ -58,6 +36,73 @@
     Agave = window.Agave;
 
     //appContext.html('<h2>Hello AIP Science App &plus; Agave API!</h2><div class="api-help list-group"></div><hr><div class="api-info"></div><br>');
+
+    var showSearchResult1 = function(json) {
+      // JavaScript === and !== operators test value and type.
+      if (json.obj.status !== 'success') {
+          console.log('Search result status is NOT good!');
+          return (false);
+      }
+      //console.log(json.obj.result);
+      var html = '<table class="table hover row-border stripe"> <thead><tr>'+
+                  '<th>KEGG Pathway ID</th> <th>KEGG Pathway Name</th> <th>Pathway Map</th>' +
+                  '</tr></thead><tbody>';
+
+      for (var i = 0; i < json.obj.result.length; i++) {
+        var entry = json.obj.result[i];
+        //console.log(json.obj.result[entry]);
+        html += '<tr><td>' + entry.identifier + '</td><td>' + entry.name +
+                '  <a role="button" data-toggle="collapse" href="#' + entry.identifier +
+                '" aria-expanded="false" aria-controls="' + entry.identifier + '" id="b' + entry.identifier + '">' +
+                '<span class="glyphicon glyphicon-collapse-down" aria-hidden="true"></a><br>\n' +
+                '<div id="' + entry.identifier + '" class="blah collapse">Loading...</div></td>' +
+                '<td><button type="button" class="btn btn-default btn-xs" '+ 'data-toggle="modal" data-target="#exampleModal" '+
+                'data-id="' + entry.identifier + '" data-name="' + entry.name + '" data-org="' + entry.organism + '">Show</button></td></tr>\n';
+      }
+      html += '</tbody></table>';
+      $('.data', appContext).html(html);
+
+
+      $('.blah').on('show.bs.collapse', function() {
+        var id = $(this).attr('id');
+        var data = $(this);
+
+        var showPathwayInfo = function(json) {
+          var fields = json.obj.result[0].fields;
+          var html = '';
+          for (var i = 0; i < fields.length; i++) {
+            html += '  - ' + fields[i] + '<br>\n';
+          }
+          data.html(html);
+
+        };
+        var query;
+        if ($('#taxonId').val() === '') {
+          query = {'identifier':id};
+        } else {
+          query = {'identifier':id, 'taxon_id': $('#taxonId').val()};
+        }
+
+
+        if ($(this).html() === 'Loading...') {
+          Agave.api.adama.search(
+                    {'namespace': 'bliu-dev',
+               'service': 'pathway_v0.2',
+               'queryParams': query},
+              showPathwayInfo,
+              showSearchError
+                );
+        }
+        $('#b' + id).html('<span class="glyphicon glyphicon-collapse-up" aria-hidden="true">');
+      });
+      $('.blah').on('hide.bs.collapse', function() {
+        var id = $(this).attr('id');
+        $('#b'+id).html('<span class="glyphicon glyphicon-collapse-down" aria-hidden="true">');
+      });
+
+
+      $('.data table', appContext).DataTable({'columnDefs': [ { 'targets': 2, 'orderable': false } ]});
+    };
 
     $('form[name=taxon_form]').on('submit', function(e) {
       e.preventDefault();
